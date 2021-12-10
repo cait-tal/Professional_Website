2b60c9aa183f3460cf3eb46c2c50af4e45a84bb7
Caitlin <cetalerico@hotmail.com>
2021-12-10 14:06:35 -0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, redirect, url_for
+from datetime import datetime
+
+app = Flask(__name__)
+
+CURRENT_YEAR = datetime.now().year
+@app.route("/")
+def redirect_home_page():
+    return redirect(url_for("home_page"))
+
+@app.route("/welcome")
+def home_page():
+    return render_template("main_base.html", year=CURRENT_YEAR)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+if __name__ == "__main__":
+    app.run(debug=True)
